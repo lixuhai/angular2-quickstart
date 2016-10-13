9ab10c3f1c1875cf1a5948b839e5aabf71403451
Lixuhai <lxh_shine@hotmail.com>
2016-10-13 10:31:21 +0800

    first commit ———— A breif structure on Angular2.0 tour of heroes

---
+++ app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent } from './app.component';
+import { HeroService } from './hero.service';
+import { HeroesComponent }     from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+import { routing } from './app.routing';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    routing
+  ],
+
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+
+  providers: [
+    HeroService
+  ],
+
+  bootstrap: [ AppComponent ]
+
+})
+export class AppModule { }
+
